1e570b8d35be302d49cd802c11b51b330520cb2a
kevinquillen <kevin@inclind.com>
2012-10-12 19:25:57 -0400

    Add in the settings passed through Drupal.settings to control where we are going. At the moment, domain: property is not working on my local machine, cookie refuses to write it. Could be because I am not using a domain with an extension (localhost). Removing it made it work.

--- js/mobile_redirect.js
+++ js/mobile_redirect.js
@@ -1,31 +1,31 @@
 (function ($) {
-    Drupal.behaviors.mobileDetection = {
+    Drupal.behaviors.mobile_redirect = {
         attach: function (context, settings) {
-            if (/DEVICE_LIST/i.test(navigator.userAgent)) {
+            if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
                 // first time visit, set the cookie and redirect
                 if ($.cookie('device') === null) {
-                    $.cookie('device', 'mobile', { domain: DOMAIN, path: '/' });
-                    window.location.replace(MOBILE_DOMAIN);
+                    $.cookie('device', 'mobile', { /*domain: Drupal.settings.mobile_js_redirect.base_domain,*/ path: '/' });
+                    window.location.replace(Drupal.settings.mobile_js_redirect.mobile_domain);
                 }
 
                 // user clicked 'view mobile site' link, set the cookie and redirect
-                if( /(device=mobile)/i.test(window.location.search) ) {
-                    $.cookie('device', 'mobile', { domain: DOMAIN, path: '/' });
-                    window.location.replace(MOBILE_DOMAIN);
+                if ( /(device=mobile)/i.test(window.location.search) ) {
+                    $.cookie('device', 'mobile', { /*domain: Drupal.settings.mobile_js_redirect.base_domain,*/ path: '/' });
+                    window.location.replace(Drupal.settings.mobile_js_redirect.mobile_domain);
                 }
 
                 // user clicked 'view full site' link, set the cookie and redirect
                 if ( /(device=desktop)/i.test(window.location.search) ) {
-                    $.cookie('device', 'desktop', { domain: DOMAIN, path: '/' });
-                    window.location.replace(DESKTOP_DOMAIN);
+                    $.cookie('device', 'desktop', { /*domain: Drupal.settings.mobile_js_redirect.base_domain,*/ path: '/' });
+                    window.location.replace(Drupal.settings.mobile_js_redirect.desktop_domain);
                 }
 
-                if ($.cookie('device') === 'desktop' && window.location.search === "" && window.location.host === MOBILE_DOMAIN) {
-                    window.location.replace(DESKTOP_DOMAIN);
+                if ($.cookie('device') === 'desktop' && window.location.search === "" && window.location.host === Drupal.settings.mobile_js_redirect.mobile_domain) {
+                    window.location.replace(Drupal.settings.mobile_js_redirect.desktop_domain);
                 }
 
-                if ($.cookie('device') === 'mobile' && window.location.search === "" && window.location.host === DESKTOP_DOMAIN) {
-                    window.location.replace(MOBILE_DOMAIN);
+                if ($.cookie('device') === 'mobile' && window.location.search === "" && window.location.host === Drupal.settings.mobile_js_redirect.desktop_domain) {
+                    window.location.replace(Drupal.settings.mobile_js_redirect.mobile_domain);
                 }
             }
         }
